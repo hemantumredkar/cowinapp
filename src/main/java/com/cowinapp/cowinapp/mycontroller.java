836a7371e4369e593999f2d1851797c500e98615
hemant.umredkar2016@gmail.com <hemant.umredkar2016@gmail.com>
2021-06-25 23:28:03 +0530

    mycontroller.java file is added

---
+++ src/main/java/com/cowinapp/cowinapp/mycontroller.java
@@ -0,0 +1,10 @@
+package com.cowinapp.cowinapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mycontroller {
+    @GetMapping("/cources")
+    public String getData() { return "Please use cowin to book vacination"; }
+}
